2ba4dc06ff71448dd3fbbcf914d6c99444613796
Akash Melachuri <amelachuri@gmail.com>
2021-04-24 13:26:17 -0400

    Added 3d flying camera

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use bevy::prelude::*;
+use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
 
 fn main() {
     App::build()
         .insert_resource(Msaa { samples: 4 })
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_plugins(DefaultPlugins)
+        .add_plugin(FlyCameraPlugin)
         .add_startup_system(setup.system())
         .run();
 }
 
-/// set up a simple 3D scene
+// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -17,19 +19,23 @@ fn setup(
 ) {
     // sphere
     commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Icosphere { radius: 1.0, ..Default::default()  })),
+        mesh: meshes.add(Mesh::from(shape::Icosphere { radius: 1.0, ..Default::default() })),
         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..Default::default()
     });
+
     // light
     commands.spawn_bundle(LightBundle {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..Default::default()
     });
+
     // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
+        .insert(FlyCamera::default());
 }
