e1f9026b799586382150b632d6212e6a20732108
Akash Melachuri <javalich@outlook.com>
2021-05-22 10:00:03 -0500

    Brute force 1000 spheres as stars

--- assets/shaders/background.frag
+++ assets/shaders/background.frag
@@ -1,53 +1,7 @@
 #version 450
 
-#define PI 3.1415
-
 layout(location = 0) out vec4 o_Target;
-layout(location = 0) in vec4 fragPos;
-layout(location = 1) in vec2 in_uv;
-
-float rand(vec2 co){
-    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-}
-
-float star(vec2 p) {
-    float d;
-    float m = 0.0;
-
-    d = length(p);
-    m += smoothstep(0.01, 0.5, 0.005 / d);
-
-    return m;
-}
-
-float atan2(in float y, in float x)
-{
-    bool s = (abs(x) > abs(y));
-    return mix(PI/2.0 - atan(x,y), atan(y,x), s);
-}
 
 void main() {
-    vec3 col = vec3(0.0);
-
-    //vec3 coord = normalize(fragPos.xyz);
-
-    //vec3 dist = coord;
-    //vec2 uv = vec2(0.5 + atan2(dist.x, dist.y)/(2. * PI), 0.5 - asin(dist.y)/PI);
-
-    vec2 uv = in_uv - 0.5;
-    uv *= 20.;
-
-    vec2 gv = fract(uv) - 0.5;
-    vec2 id = floor(uv);
-
-    for (float y = -1.; y < 2.; y++) {
-        for (float x = -1.; x < 2.; x++) {
-            vec2 offset = vec2(x, y);
-            float n = rand(id + offset);
-
-            col += star(gv - offset -vec2(n, fract(n*34.)-.5));
-        }
-    }
-
-    o_Target = vec4(abs(uv), 0.0, 1.0);
+    o_Target = vec4(1.0);
 }
--- src/background/mod.rs
+++ src/background/mod.rs
@@ -2,13 +2,14 @@ use bevy::{
     prelude::*,
     reflect::TypeUuid,
     render::{
-        mesh::Indices,
-        pipeline::{PipelineDescriptor, PrimitiveTopology, RenderPipeline},
+        pipeline::{PipelineDescriptor, RenderPipeline},
         render_graph::{base, AssetRenderResourcesNode, RenderGraph},
         renderer::RenderResources,
         shader::ShaderStages,
     },
 };
+
+use rand_distr::{Distribution, UnitSphere};
 
 #[derive(RenderResources, Default, TypeUuid)]
 #[uuid = "0dbbc306-413c-4a2f-8f24-4a8212205619"]
@@ -24,61 +25,20 @@ pub fn setup_background(
 ) {
     asset_server.watch_for_changes().unwrap();
 
+    let mut rng = rand::thread_rng();
+
+    let stars: Vec<Vec3> = UnitSphere
+        .sample_iter(&mut rng)
+        .take(1000)
+        .map(|xyz| 800.0 * Vec3::new(xyz[0], xyz[1], xyz[2]))
+        .collect();
+
     let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
         vertex: asset_server.load::<Shader, _>("shaders/background.vert"),
         fragment: Some(asset_server.load::<Shader, _>("shaders/background.frag")),
     }));
 
-    let size = 1.;
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.set_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        vec![
-            [0.0, size, 0.0],
-            [0.0, 0.0, 0.0],
-            [size, size, 0.0],
-            [size, 0.0, 0.0],
-            [0.0, 0.0, size],
-            [size, 0.0, size],
-            [0.0, size, size],
-            [size, size, size],
-            [0.0, size, 0.0],
-            [size, size, 0.0],
-            [0.0, size, 0.0],
-            [0.0, size, size],
-            [size, size, 0.0],
-            [size, size, size],
-        ],
-    );
-    mesh.set_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![
-            [0.0, 0.66],
-            [0.25, 0.66],
-            [0.0, 0.33],
-            [0.25, 0.33],
-            [0.5, 0.66],
-            [0.5, 0.33],
-            [0.75, 0.66],
-            [0.75, 0.33],
-            [1.0, 0.66],
-            [1.0, 0.33],
-            [0.25, 1.0],
-            [0.5, 1.0],
-            [0.25, 0.0],
-            [0.5, 0.0],
-        ],
-    );
-    mesh.set_indices(Some(Indices::U32(vec![
-        0, 2, 1, 1, 2, 3, 4, 5, 6, 5, 7, 6, 6, 7, 8, 7, 9, 8, 1, 3, 4, 3, 5, 4, 1, 11, 10, 1, 4,
-        11, 3, 12, 5, 5, 12, 13,
-    ])));
-
     let material = custom_materials.add(BackgroundMaterial);
-
-    let size = 500.;
-    let mut transform = Transform::from_xyz(size / 2., size / 2., size / 2.);
-    transform.scale = Vec3::new(-size, -size, -size);
 
     render_graph.add_system_node(
         "background_material",
@@ -89,14 +49,19 @@ pub fn setup_background(
         .add_node_edge("background_material", base::node::MAIN_PASS)
         .unwrap();
 
-    commands
-        .spawn_bundle(MeshBundle {
-            mesh: meshes.add(mesh),
-            render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
-                pipeline_handle,
-            )]),
-            transform,
-            ..Default::default()
-        })
-        .insert(material);
+    stars.into_iter().for_each(|s| {
+        commands
+            .spawn_bundle(MeshBundle {
+                mesh: meshes.add(Mesh::from(shape::Icosphere {
+                    radius: 1.,
+                    subdivisions: 2,
+                })),
+                transform: Transform::from_translation(s),
+                render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
+                    pipeline_handle.clone(),
+                )]),
+                ..Default::default()
+            })
+            .insert(material.clone());
+    });
 }
