6dc4bb468e4e616f8f9b67772d00aad0e86eaa2a
Akash Melachuri <amelachuri@gmail.com>
2021-05-26 17:17:01 -0500

    Generating cube vertices

--- assets/shaders/planet.frag
+++ assets/shaders/planet.frag
@@ -1,7 +1,9 @@
 #version 450
 
+layout(location = 1) in vec2 uv;
+
 layout(location = 0) out vec4 o_Target;
 
 void main() {
-    o_Target = vec4(1.0);
+    o_Target = vec4(1.);
 }
--- src/generation/planet/mod.rs
+++ src/generation/planet/mod.rs
@@ -2,7 +2,8 @@ use bevy::{
     prelude::*,
     reflect::TypeUuid,
     render::{
-        pipeline::{PipelineDescriptor, RenderPipeline},
+        mesh::Indices,
+        pipeline::{PipelineDescriptor, PrimitiveTopology, RenderPipeline},
         render_graph::{base, AssetRenderResourcesNode, RenderGraph},
         renderer::RenderResources,
         shader::ShaderStages,
@@ -12,6 +13,51 @@ use bevy::{
 #[derive(RenderResources, Default, TypeUuid)]
 #[uuid = "aa030eb0-c8cb-4ce3-9faa-0f74d4be2280"]
 pub struct PlanetMaterial;
+
+fn generate_mesh() -> Mesh {
+    let size = 1.;
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+    let verts: Vec<[f32; 3]> = (0..8)
+        .map(|i| {
+            [
+                if i & 4 != 0 { size } else { -size },
+                if i & 2 != 0 { size } else { -size },
+                if i & 1 != 0 { size } else { -size },
+            ]
+        })
+        .collect();
+
+    let mut tris: Vec<[f32; 3]> = Vec::new();
+    for i in 0..3 {
+        let v1 = 1 << i;
+        let v2 = if v1 == 4 { 1 } else { v1 << 1 };
+
+        tris.push(verts[0]);
+        tris.push(verts[v1]);
+        tris.push(verts[v2]);
+        tris.push(verts[v1 + v2]);
+        tris.push(verts[v2]);
+        tris.push(verts[v1]);
+        tris.push(verts[7]);
+        tris.push(verts[7 - v2]);
+        tris.push(verts[7 - v1]);
+        tris.push(verts[7 - (v1 + v2)]);
+        tris.push(verts[7 - v1]);
+        tris.push(verts[7 - v2]);
+    }
+
+    let len = tris.len();
+
+    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, tris);
+    mesh.set_indices(Some(Indices::U32((0..36).collect())));
+
+    let uv: Vec<[f32; 2]> = (0..len).map(|_x| [0.; 2]).collect();
+    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uv);
+
+    mesh
+}
 
 pub fn setup_planets(
     mut commands: Commands,
@@ -29,7 +75,6 @@ pub fn setup_planets(
     }));
 
     let material = custom_materials.add(PlanetMaterial);
-
     render_graph.add_system_node(
         "planet_material",
         AssetRenderResourcesNode::<PlanetMaterial>::new(true),
@@ -41,12 +86,9 @@ pub fn setup_planets(
 
     commands
         .spawn_bundle(MeshBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: 0.5,
-                ..Default::default()
-            })),
+            mesh: meshes.add(generate_mesh()),
             render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
-                pipeline_handle.clone(),
+                pipeline_handle,
             )]),
             transform: Transform::from_xyz(-20.0, 8.0, 4.0),
             ..Default::default()
